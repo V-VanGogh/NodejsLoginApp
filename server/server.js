724d6e98a475c8dab8dd5ebd1791b9ec0951245d
Nikos <nmathioudis@gmail.com>
2019-01-11 19:00:23 +0200

    first commit

--- server/server.js
+++ server/server.js
@@ -6,25 +6,48 @@ const bodyparser = require('body-parser');
 const _ = require("underscore");
 const {ObjectID} = require('mongodb');
 
+var router = express.Router();
+
 
 
 //local imports
 var {mongoose} = require('./db/mongoose');
 var {User}= require('./models/user');
 var {authenticate} = require('./middleware/authenticate');
-
-
 var app = express();
+// var api = require('api');
+
+// app.use(express.static(__dirname + './../node_modules'));
+// prepare server
+// app.use('/api', api); // redirect API calls
+app.use('/js', express.static(__dirname + './../node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/Js', express.static(__dirname + './../node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + './../node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/myCss',express.static(__dirname + '/public'));//redirect for my css
+
+
+
+var path = __dirname + '/views';
+router.get('/', (req,res) => {
+  res.sendFile(path + '/index.html');
+});
+
+app.use('/',router);
+
+
 const port = process.env.PORT || 3000;
 
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 
 // POST /users
 app.post('/users', (req,res) => {
-  var body = _.pick(req.body, ['email','password']);
+  if(req.body.password == req.body.passwordConfirm) {
+  var arr = ['email', 'password'];
+  var body = _.pick(req.body, arr);
+  console.log(arr,body);
   var user = new User(body);
-
 
   user.save().then((user) => {
     return user.generateAuthToken();
@@ -34,7 +57,13 @@ app.post('/users', (req,res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+}
+else if(req.body.password != req.body.passwordConfirm) {
+  res.redirect('/?e=' + encodeURIComponent('Passwords must be the same'));
+}
 });
+
+
 
 //Make a private route
 // Get /users/me
@@ -43,6 +72,9 @@ app.get('/users/me',authenticate, (req,res) => {
   res.send(req.user);
 });
 
+router.get('/about', (req,res) => {
+  res.sendFile(path + '/about.html');
+});
 
 
 
